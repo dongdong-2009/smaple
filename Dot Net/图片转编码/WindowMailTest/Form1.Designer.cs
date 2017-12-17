e0791eba877c569077ed96f6c312b2684c6a0833
amingo123 <31767702@qq.com>
2017-12-17 17:21:22 +0800

    auto commit

---
+++ Dot Net/图片转编码/WindowMailTest/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowMailTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_encode = new System.Windows.Forms.Button();
+            this.txt_code = new System.Windows.Forms.TextBox();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_encode
+            // 
+            this.btn_encode.Location = new System.Drawing.Point(13, 13);
+            this.btn_encode.Name = "btn_encode";
+            this.btn_encode.Size = new System.Drawing.Size(75, 23);
+            this.btn_encode.TabIndex = 0;
+            this.btn_encode.Text = "将图片编码";
+            this.btn_encode.UseVisualStyleBackColor = true;
+            this.btn_encode.Click += new System.EventHandler(this.btn_encode_Click);
+            // 
+            // txt_code
+            // 
+            this.txt_code.Location = new System.Drawing.Point(13, 43);
+            this.txt_code.Multiline = true;
+            this.txt_code.Name = "txt_code";
+            this.txt_code.Size = new System.Drawing.Size(802, 472);
+            this.txt_code.TabIndex = 1;
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(117, 13);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(281, 23);
+            this.btn_send.TabIndex = 2;
+            this.btn_send.Text = "将转码后的图片作为邮件正文发送出去";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 527);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.txt_code);
+            this.Controls.Add(this.btn_encode);
+            this.Name = "Form1";
+            this.Text = "发送正文带图片邮件";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_encode;
+        private System.Windows.Forms.TextBox txt_code;
+        private System.Windows.Forms.Button btn_send;
+    }
+}
+
---
+++ Dot Net/图片转编码/WindowMailTest/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowMailTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_encode_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Multiselect = false;
+            dlg.Title = "选择要转换的图片";
+            dlg.Filter = "Image files (*.jpg;*.bmp;*.gif;*.png)|*.jpg;*.jpeg;*.gif;*.bmp|AllFiles (*.*)|*.*";
+            if (DialogResult.OK == dlg.ShowDialog())
+            {
+
+                  txt_code.Text=  ImgToBase64String(dlg.FileName);
+            }
+        }
+        /// <summary>
+        /// 将图片转为base64编码的字符串
+        /// </summary>
+        /// <param name="Imagefilename">图片路径</param>
+        /// <returns></returns>
+        private string ImgToBase64String(string Imagefilename)
+        {
+
+            Bitmap bmp = new Bitmap(Imagefilename);
+
+            MemoryStream ms = new MemoryStream();
+            bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            byte[] arr = new byte[ms.Length];
+            ms.Position = 0;
+            ms.Read(arr, 0, (int)ms.Length);
+            ms.Close();
+            return Convert.ToBase64String(arr);
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            string mailBody = "<BODY style=\"MARGIN: 10px\"><DIV><IMG src=\"data:image/png;base64,"+txt_code.Text+"\"> </IMG></DIV></BODY> ";
+            System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient("smtp.163.com");
+            client.UseDefaultCredentials = true;//设置为发送认证消息
+            client.Credentials = new System.Net.NetworkCredential("用户名", "密码");//认证消息
+            System.Net.Mail.MailMessage mess = new System.Net.Mail.MailMessage();
+            mess.From = new System.Net.Mail.MailAddress("shiyeping@163.com", "要显示的发信人的名称");
+            mess.To.Add(new System.Net.Mail.MailAddress("shiyeping@163.com", "要显示的收信人的名称"));
+            mess.Subject = "主题";
+            mess.IsBodyHtml = true;
+            mess.Body = mailBody;
+            try
+            {
+                client.Send(mess);
+                MessageBox.Show("发送成功完成");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
